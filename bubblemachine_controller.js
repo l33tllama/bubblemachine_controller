86f56e82ad54b028ae2521d5738d458cc55dc357
l33tllama <overclockedamiga@gmail.com>
2016-07-31 15:40:26 +1000

    starting and stopping pins..2

--- bubblemachine_controller.js
+++ bubblemachine_controller.js
@@ -16,10 +16,11 @@ function closePins() {
 }
 closePins();
 
-gpio.on('export', function(channel) {
-    console.log('Channel set: ' + channel);
-});
-
+gpio.setup(sc1A, gpio.DIR_OUT);
+gpio.setup(sc1B, gpio.DIR_OUT);
+gpio.setup(sc2A, gpio.DIR_OUT);
+gpio.setup(sc2B, gpio.DIR_OUT);
+gpio.setup(fanPin, gpio.DIR_OUT);
 
 
 console.log("Pin setup complete");
@@ -111,11 +112,7 @@ function moveStepper(steps, callback){
 
 var MachineController = {
 	emit : function(qty, callback){
-		gpio.setup(sc1A, gpio.DIR_OUT);
-		gpio.setup(sc1B, gpio.DIR_OUT);
-		gpio.setup(sc2A, gpio.DIR_OUT);
-		gpio.setup(sc2B, gpio.DIR_OUT);
-		gpio.setup(fanPin, gpio.DIR_OUT);
+		
 		
 		async.series([
 			function(callback){
